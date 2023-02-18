361b1c8bc0eaee3bbdf1cd7c42ca5db8fe7ee3f2
Daniel Mevs <danielmevs@Daniels-MacBook-Pro.local>
2023-02-18 17:41:07 -0500

    completed day sixteen

--- Days/day16/main.py
+++ Days/day16/main.py
@@ -1,7 +1,22 @@
-import another_module
+# import another_module
+# print(another_module.another_variable)
 
-print(another_module.another_variable)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
-import turtle
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = turtle.Turtle()
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+
+print(table)
---
+++ Days/day16/oop-coffee-machine-start/main.py
@@ -0,0 +1,25 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+coffee_maker.report()
+money_machine.report()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        print(drink)
