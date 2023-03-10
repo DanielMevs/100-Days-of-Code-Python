7ac24d9dea63889285a01a2719e7a60800690841
DanielMevs <dannymevs@hotmail.com>
2023-03-10 17:05:30 -0500

    finished day twenty four

---
+++ Days/day24/files.py
@@ -0,0 +1,17 @@
+# - Appends/adds to the existing file without deleting anything
+# with open('my_text.txt', mode="a") as file:
+#     file.write('\nNew text.')
+
+
+# - Overwrites what's in the existing file
+# with open('my_text.txt', mode="w") as file:
+#     file.write('New text.')
+
+# with open('my_text.txt') as file:
+#     contents = file.read()
+#     print(contents)
+
+# file = open("my_text.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
