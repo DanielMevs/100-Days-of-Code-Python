308abe9e3f8403510f71287179fd534ad40e99fb
DanielMevs <dannymevs@hotmail.com>
2023-04-18 16:44:24 -0400

    completed day 57

--- Days/day57/server.py
+++ Days/day57/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -12,7 +13,24 @@ def home():
 
 @app.route("/guess/<name>")
 def guess(name):
-    return render_template("guess.html", name=name)
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    return render_template("guess.html", name=name, gender=gender, age=age)
+
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    print(num)
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
