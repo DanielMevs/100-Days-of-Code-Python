20d562f8297b4ff4e08065b844c65e79c1049df6
DanielMevs <dannymevs@hotmail.com>
2023-05-04 22:08:47 -0400

    completed day 63

--- Days/day63/main.py
+++ Days/day63/main.py
@@ -1,12 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+# - Creating DB
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# - Create a Table
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+    
+db.create_all()
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
@@ -18,13 +36,37 @@ def add():
             "author": request.form["author"],
             "rating": request.form["rating"]
         }
-        all_books.append(new_book)
+        db.session.add(new_book)
+        db.session.commit()
         
-        #NOTE: You can use the redirect method from flask to redirect to another route 
-        # e.g. in this case to the home page after the form has been submitted.
         return redirect(url_for('home'))
       
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # - Update Record
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # - Delete a Record by ID
+    book_to_delete = Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
