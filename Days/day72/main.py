30f067f9ea7c0765a45baddeb5b56a896bf3240c
DanielMevs <dannymevs@hotmail.com>
2023-06-01 17:01:48 -0400

    continued day 72

--- Days/day72/main.py
+++ Days/day72/main.py
@@ -24,13 +24,35 @@ df = pd.read_csv('./QueryResults.csv', names=['DATE', 'TAG', 'POSTS'], header=0)
 # print(df.groupby('TAG').count())
 
 # - We can use square brackets to access an entry in a column
-print(df['DATE'][2])
+# print(df['DATE'][2])
 # - alternatively
-print(df.DATE[1])
+# print(df.DATE[1])
 
 # - To inspect the datatype of an entry
-print(type(df['DATE'][1]))
+# print(type(df['DATE'][1]))
 
 # - We see that our entry is of type string.
 # - To convert to a date-time object
-print(type(pd.to_datetime(df.DATE[1])))
+# print(type(pd.to_datetime(df.DATE[1])))
+
+
+test_df = pd.DataFrame({'Age': ['Young', 'Young', 'Young', 'Young', 'Old', 'Old', 'Old', 'Old'],
+                        'Actor': ['Jack', 'Arnold', 'Keanu', 'Sylvester', 'Jack', 'Arnold', 'Keanu', 'Sylvester'],
+                        'Power': [100, 80, 25, 50, 99, 75, 5, 30]})
+print(test_df)
+
+# - If we want to convert our dataframe such that each category of actor has his own column: 
+pivoted_df = test_df.pivot(index='Age', columns='Actor', values='Power')
+print(pivoted_df)
+
+# - To pivot the df DataFrame so that each row is a date and each column is a programming language:
+reshaped_df = df.pivot(index='DATE', columns='TAG', values='POSTS')
+print(reshaped_df)
+
+# - To substitute all values that have NaN with 0:
+reshaped_df = reshaped_df.fillna(0) 
+print(reshaped_df.head())
+
+# - To check if there are any NaN values left in the entire DataFrame:
+is_nan_left = reshaped_df.isna().values.any()
+print(is_nan_left)
