1a4ccbf73493cae3959ba5aebbf014f833174da9
DanielMevs <dannymevs@hotmail.com>
2023-06-17 18:52:23 -0400

    completed day 81

---
+++ Days/day81/main.py
@@ -0,0 +1,33 @@
+# Dictionary representing the morse code chart
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-', ' ': '/'}
+
+
+# Function to encrypt the string
+# according to the morse code chart
+def encrypt(message):
+    return " ".join(MORSE_CODE_DICT[char] for char in message.upper())
+
+
+while True:
+    msg = input("Enter a message to convert into morse code (n to escape): ")
+    if msg.lower() == 'n':
+        break
+    else:
+        translation = encrypt(msg)
+        print(f'Your message in morse code: {translation}')
+    
+
