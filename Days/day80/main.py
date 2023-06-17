b9206f9d34f08a1d275dc8d66457a3af31348d0e
DanielMevs <dannymevs@hotmail.com>
2023-06-16 21:17:23 -0400

    continued day 80

--- Days/day80/main.py
+++ Days/day80/main.py
@@ -152,15 +152,172 @@ You can make your life easier by providing a list of
 values for the x-axis (e.g., `x=['No', 'Yes']`)
 '''
 
-river_access = data['CHAS'].value_counts()
+# river_access = data['CHAS'].value_counts()
 
-bar = px.bar(x=['No', 'Yes'],
-             y=river_access.values,
-             color=river_access.values,
-             color_continuous_scale=px.colors.sequential.haline,
-             title='Next to Charles River?')
+# bar = px.bar(x=['No', 'Yes'],
+#              y=river_access.values,
+#              color=river_access.values,
+#              color_continuous_scale=px.colors.sequential.haline,
+#              title='Next to Charles River?')
 
-bar.update_layout(xaxis_title='Property Located Next to the River?', 
-                  yaxis_title='Number of Homes',
-                  coloraxis_showscale=False)
-bar.show()
+# bar.update_layout(xaxis_title='Property Located Next to the River?', 
+#                   yaxis_title='Number of Homes',
+#                   coloraxis_showscale=False)
+# bar.show()
+
+
+# Understand the Relationships in the Data
+
+'''**Challenge**
+
+There might be some relationships in the data that we should know about. Before you run the code, make some predictions:
+
+* What would you expect the relationship to be between pollution (NOX) and the distance to employment (DIS)? 
+* What kind of relationship do you expect between the number of rooms (RM) and the home value (PRICE)?
+* What about the amount of poverty in an area (LSTAT) and home prices? '''
+
+
+# sns.pairplot(data)
+
+# # You can even include a regression line
+# # sns.pairplot(data, kind='reg', plot_kws={'line_kws':{'color': 'cyan'}})
+# plt.show()
+
+
+# **Challenge**
+
+'''Use Seaborn's .jointplot() to look at some of the relationships 
+in more detail. Create a jointplot for:
+
+* DIS and NOX
+* INDUS vs NOX
+* LSTAT vs RM
+* LSTAT vs PRICE
+* RM vs PRICE
+
+Try adding some opacity or `alpha` to the scatter plots using keyword
+ arguments under `joint_kws`.
+ 
+ '''
+
+# with sns.axes_style('darkgrid'):
+#   sns.jointplot(x=data['DIS'], 
+#                 y=data['NOX'], 
+#                 height=8, 
+#                 kind='scatter',
+#                 color='deeppink', 
+#                 joint_kws={'alpha':0.5})
+
+# plt.show()
+
+# with sns.axes_style('darkgrid'):
+#   sns.jointplot(x=data.NOX, 
+#                 y=data.INDUS, 
+#                 # kind='hex', 
+#                 height=7, 
+#                 color='darkgreen',
+#                 joint_kws={'alpha':0.5})
+# plt.show()
+
+
+
+
+#### % of Lower Income Population vs Average Number of Rooms
+
+
+'''
+
+*Challenge** 
+
+Compare LSTAT (proportion of lower-income population)
+with RM (number of rooms) using Seaborn's `.jointplot()`.
+How does the number of rooms per dwelling vary with the
+ poverty of area? Do homes have more or fewer rooms when LSTAT is low?
+
+'''
+
+
+# with sns.axes_style('darkgrid'):
+#   sns.jointplot(x=data['LSTAT'], 
+#                 y=data['RM'], 
+#                 # kind='hex', 
+#                 height=7, 
+#                 color='orange',
+#                 joint_kws={'alpha':0.5})
+# plt.show()
+
+
+'''**Challenge**
+
+Compare LSTAT with PRICE using Seaborn's `.jointplot()`. How does the
+ proportion of the lower-income population in an area affect home prices?'''
+
+
+# with sns.axes_style('darkgrid'):
+#   sns.jointplot(x=data.LSTAT, 
+#                 y=data.PRICE, 
+#                 # kind='hex', 
+#                 height=7, 
+#                 color='crimson',
+#                 joint_kws={'alpha':0.5})
+# plt.show()
+
+#### Number of Rooms versus Home Value
+
+'''**Challenge** 
+
+Compare RM (number of rooms) with PRICE using Seaborn's `.jointplot()`.
+ You can probably guess how the number of rooms affects home prices. 😊 '''
+
+
+# with sns.axes_style('whitegrid'):
+#   sns.jointplot(x=data.RM, 
+#                 y=data.PRICE, 
+#                 height=7, 
+#                 color='darkblue',
+#                 joint_kws={'alpha':0.5})
+# plt.show()
+
+
+# Split Training & Test Dataset
+
+'''
+
+**Challenge**
+
+* Import the `train_test_split()` function from sklearn
+* Create 4 subsets: X_train, X_test, y_train, y_test
+* Split the training and testing data roughly 80/20. 
+* To get the same random split every time you run your 
+notebook use `random_state=10`. This helps us get the same results
+ every time and avoid confusion while we're learning. 
+
+'''
+
+target = data['PRICE']
+features = data.drop('PRICE', axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(features, 
+                                                    target, 
+                                                    test_size=0.2, 
+                                                    random_state=10)
+
+
+# % of training set
+train_pct = 100*len(X_train)/len(features)
+print(f'Training data is {train_pct:.3}% of the total data.')
+
+# % of test data set
+test_pct = 100*X_test.shape[0]/features.shape[0]
+print(f'Test data makes up the remaining {test_pct:0.3}%.')
+
+
+
+# Multivariable Regression
+
+'''
+**Challenge**
+
+Use sklearn to run the regression on the training dataset.
+ How high is the r-squared for the regression on the training data?
+'''
